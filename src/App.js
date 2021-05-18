baf8287feea61d7a4cc9226e35d4a3cd67f8c985
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2021-05-17 17:42:25 -0700

    added more components and connected to API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+import RecipeTile from "./components/recipe-tile";
+import Axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [query, setquery] = useState("");
+  const [recipes, setrecipes] = useState([]);
+
+  const YOUR_APP_ID = `e22da61e`;
+  const YOUR_APP_KEY = "73cfc1471c869bd218c91d8067d85820";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+
+  const getRecipeInfo = async () => {
+    var result = await Axios.get(url);
+    setrecipes(result.data.hits);
+    console.log(result.data.hits);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipeInfo();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 onClick={getRecipeInfo}>Food Recipe Plaza 🍔</h1>
+      <form className="app__searchForm" onSubmit={onSubmit}>
+        <input
+          className="app__input"
+          type="text"
+          placeholder="enter ingridient"
+          autoComplete="Off"
+          value={query}
+          onChange={(e) => setquery(e.target.value)}
+        />
+        <input className="app__submit" type="submit" value="Search" />
+      </form>
+
+      <div className="app__recipes">
+        {recipes !== [] &&
+          recipes.map((recipe) => {
+            return <RecipeTile recipe={recipe} />;
+          })}
+      </div>
     </div>
   );
 }
